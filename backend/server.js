35786672a5a11a263d755feb12cd00c898f239c1
BenMiller0 <bem002@ucsd.edu>
2025-07-02 14:45:03 -0700

    inital commit

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.error(err));
+
+app.get('/api', (req, res) => {
+  res.send('API running');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
